c1f398b1fd4b4efbd03fc6355b37b88c0167f7e1
Francis <mingomba@gmail.com>
2023-09-26 02:34:18 +0000

    Hash password

--- src/main/java/com/example/java/rest/api/service/AuthenticationServiceImpl.java
+++ src/main/java/com/example/java/rest/api/service/AuthenticationServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import com.example.java.rest.api.error.DuplicateUserException;
 import com.example.java.rest.api.model.User;
@@ -19,12 +20,24 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         if (alreadyExists(userFromClientUser)) {
             throw new DuplicateUserException(" User already exists!!!!");
         }
+
+        String passwordg = password(userFromClientUser);
+        userFromClientUser.setPassword(passwordg);
+
         return this.userRepository.save(userFromClientUser);
 
     }
 
     private boolean alreadyExists(User user) {
         return this.userRepository.findUserByEmail(user.getEmail()) != null;
+    }
+
+    public String password(User user) {
+
+        String hashedPassword = BCrypt.hashpw(user.getPassword(),
+                BCrypt.gensalt(10));
+        return hashedPassword;
+
     }
 
     @Override

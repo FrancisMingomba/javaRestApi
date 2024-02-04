2f42b042ffd48b5c03d622b4b5eb3736db5d57d1
Francis <mingomba@gmail.com>
2024-02-04 09:32:13 +0000

    Other commit

--- src/main/java/com/example/java/rest/api/api/UserApi.java
+++ src/main/java/com/example/java/rest/api/api/UserApi.java
@@ -3,8 +3,6 @@ package com.example.java.rest.api.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/example/java/rest/api/repository/UserRepository.java
+++ src/main/java/com/example/java/rest/api/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.java.rest.api.repository;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.java.rest.api.model.User;
 
